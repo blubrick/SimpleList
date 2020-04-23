5b5da2f611832ad0c854d79832976ca146e5b2b7
Matt Livingston <blubrick@gmail.com>
2020-04-23 12:27:06 +1000

    Episode 8 - reading from and writing to files

---
+++ src/include/database.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Database {
+    private:
+    // Accessible to his class only
+
+    protected:
+    // Accessible to this class and its descendants
+
+    public:
+    // Accessible to all
+    Database() {
+        // Constructor - Signified by its name 
+        // being the same as the Class name
+        // Initialise any required values 
+        // in this method
+    }
+    ~Database() {
+        // Destructor - Signified by the name 
+        // being the same as the Class name, 
+        // but with a leading "~" 
+        // Cleanup code for the object goes 
+        // in this method
+    }
+    vector<string> list;
+    string name;
+
+    void write(vector<string> list);
+    void read();
+};
